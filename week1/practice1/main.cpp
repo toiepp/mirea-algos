3bf099170bf9d083e5c96bb038f9e239663a7524
Ivan Mikholskiy <ivanmikhmain@gmail.com>
2021-09-06 12:49:29 +0300

    prase bitset left

--- week1/practice1/main.cpp
+++ week1/practice1/main.cpp
@@ -1,7 +1,8 @@
 #include <bitset>
 #include <iostream>
 
-#define TOP
+//#define TOP
+
 #ifdef TOP
 const int MAX = 8;
 #else
@@ -97,6 +98,8 @@ void bit_sort() {
 	print(arr, size);
 }
 
+#ifndef TOP
+// Данная сортировка работает для массивов размером от 0 до 64
 void bit_sort_with_chars() {
 	std::cout << "Задание 2.в" << std::endl;
 	// Создаю массив чисел
@@ -104,17 +107,46 @@ void bit_sort_with_chars() {
 	std::cout << "Введите размер массива: ";
 	std::cin >> size;
 	int *arr = new int[size];
-	std::cout << "Введите массив (8, 0-7): ";
+	std::cout << "Введите массив (64, 0-63): ";
 	for (int i = 0; i < size; ++i) {
 		std::cin >> arr[i];
 	}
 
-	// Создаю битовый массив, равный 64 битам
+	// Создаю битовый массив, равный 64 битам,
+	// в котором все поля равны 0
+	const int size_of_bit_set = 8; // размер битового массива в байтах
 	auto *bitset = new unsigned char[8];
-	for (int i = 0; i < 8; ++i) {
+	for (int i = 0; i < size_of_bit_set; ++i) {
 		bitset[i] = 0;
 	}
+
+	// Заполняю битовый массив
+	for (int i = 0; i < size; ++i) {
+		unsigned char mask = 1;
+		// позиция в массиве bitset
+		int pos_in_array = arr[i] / size_of_bit_set;
+		// позиция в элемента массива bitset
+		int pos_in_element = arr[i] % size_of_bit_set;
+		bitset[pos_in_array] = ~(~bitset[pos_in_array] & ~(mask << pos_in_element));
+	}
+
+	// Печатаю битовый массив
+	for (int i = 0; i < 8; ++i) {
+		for (int j = 0; j < 8; ++j) {
+			std::cout << ((bitset[i] >> j) & 1);
+		}
+		std::cout << " ";
+	}
+
+	// Обхожу битовый массив
+	int zero_count = 0;
+	for (int i = 0; i < size_of_bit_set; ++i) {
+		for (int j = 0; j < 8; ++j) {
+
+		}
+	}
 }
+#endif
 
 int main() {
 	// Задание 1.а
@@ -126,7 +158,14 @@ int main() {
 	// Задание 1.в
 	one_c();
 
-	bit_sort();
+//	bit_sort();
+	bit_sort_with_chars();
+
+//	unsigned char c = 0b10101010;
+//	for (int i = 7; i >= 0; --i) {
+//		std::cout << ((c >> i) & 0x1) << " ";
+//	}
+//	std::cout << std::endl;
 
 	return 0;
 }
