38427c8db491fc189002454d569acc2812a6a323
Ivan Mikholskiy <ivanmikhmain@gmail.com>
2021-09-26 12:08:49 +0300

    to pull from pc

--- week2/practice2/HashSet.cpp
+++ week2/practice2/HashSet.cpp
@@ -1,32 +1,36 @@
 #include "HashSet.h"
 
-HashSet::HashSet() {
-	for (int i = 0; i < capacity; ++i) {
-		arr[i] = nullptr;
-	}
-}
+HashSet::HashSet() = default;
 
-HashSet::HashSet(int initial_capacity)
-	: capacity(initial_capacity) {}
+HashSet::HashSet(int initial_capacity) : capacity(initial_capacity) {}
 
-HashSet::HashSet(int initial_capacity, double load_factor)
-	: HashSet(initial_capacity) {
+HashSet::HashSet(int initial_capacity, double load_factor) : HashSet(initial_capacity) {
 	this->load_factor = load_factor;
 }
 
 void HashSet::put(Account &account) {
-	// Проверить заполнение массива
-	// Если заполнение массива станет больше допустимой нормы,
-	// то нежно провести перераспределение элементов
-	if (((double) (100 * size) / capacity) > this->load_factor) {
+	// Проверить заполнение массива, если надо перераспределить массив
+	// WARN По-моему неправильно работает определение заполнения списка
+	if (((double) (100 * (size + 1)) / capacity) > this->load_factor) {
 		rehash();
 	}
-	// Проверяю заполнена ли ячейка в массиве с полученным хэшкодом
+	// Получаю позицию, в которую должен вставиться элемент
 	int pos = f(account);
-	// Если заполнена, то получаю новую позицию нового элемента
-	if (arr[pos] != nullptr) {
-		// добавить в map кол-во хэщей
-		pos = g(account);
+	// Если ячейка не занята, то кладу в нее элемент.
+	// И добавляю по данному ключу счетчик
+	if (arr.at(pos) == nullptr) {
+		arr[pos] = &account;
+		shifts.insert(std::pair<int, int>(pos, 1));
+		return;
+	}
+	// Проверяю заполнена ли ячейка в массиве с полученным хэшкодом
+	// Если ячейка занята, то получаю новую позицию нового элемента
+	if (arr.at(pos) != nullptr) {
+		// Получаю новую позицию элемента в таблице
+		shifts.at(pos)++;
+		pos = g(account); // WARN какая-то хуйня
+		arr[pos] = &account;
+		// Увеличиваю счетчик по данному хэш-коду
 	}
 }
 
@@ -39,15 +43,29 @@ bool HashSet::find(Account &account) const {
 }
 
 void HashSet::print() const {
+	for (int i = 0; i < capacity; ++i) {
+		if (arr.at(i) != nullptr) {
+			std::cout << "(" << i << ", " << (*arr.at(i)).hash_code() << "): " << (*arr.at(i)) << std::endl;
+		}
+	}
 }
 
 void HashSet::rehash() {
 }
 
 int HashSet::f(const Account &account) const {
-	return static_cast<int>(account.hash_code()) % this->capacity;
+	int hash_code = abs(((int) account.hash_code()));
+	return hash_code % this->capacity;
 }
 
 int HashSet::g(const Account &account) const {
-	return static_cast<int>((f(account) + hash_codes_count.at((int) account.hash_code()) * (1 + account.hash_code() % (capacity - 2))));
+	/*
+	 * k - ключ поиска (хэшкод элемента)
+	 * i - счетчик (кол-во элементов с одинаковым хэшкодом)
+	 * N - вместимость структуры данных
+	 * */
+	int init_pos = f(account);                   // изначальная позиция элемента
+	int i = shifts.at((int) account.hash_code());// кол-во элементов с таким же хэш кодом
+	int k = (int) account.hash_code();           // хэш код элемента
+	return (int) (init_pos + i * (1 + k % (capacity - 2)));
 }
--- week2/practice2/main.cpp
+++ week2/practice2/main.cpp
@@ -4,5 +4,45 @@ int main() {
 	std::cout << "Вариант 13" << std::endl;
 	std::cout << "Открытая адресация (Двойное хеширование)" << std::endl;
 
+	HashSet set = HashSet();
+
+	auto a1 = Account("Ivan", "Nagornaya");
+	auto a2 = Account("fjabfk", "ajkbfhkalfb");
+	auto a3 = Account("qegq", "tttrAAT");
+	auto a4 = Account("gtwhwh", ";ajbv");
+	auto a5 = Account("gqrhteshe", "khzabfl");
+	auto a6 = Account("arlsdngsnl", "aya");
+	auto a7 = Account("jfagvlarba", "ahrahr");
+	auto a8 = Account("cdajrbflva", "ejgbvvlsk");
+	auto a9 = Account("fejdnjdn", "fncbflfakfb");
+	// Выскакивает ошибка в map out_of_range
+	set.put(a1);
+	set.print();
+	std::cout << std::endl;
+	set.put(a2);
+	set.print();
+	std::cout << std::endl;
+	set.put(a3);
+	set.print();
+	std::cout << std::endl;
+	set.put(a4);
+	set.print();
+	std::cout << std::endl;
+	set.put(a5);
+	set.print();
+	std::cout << std::endl;
+	set.put(a6);
+	set.print();
+	std::cout << std::endl;
+	set.put(a7);
+	set.print();
+	std::cout << std::endl;
+	set.put(a8);
+	set.print();
+	std::cout << std::endl;
+	set.put(a9);
+	set.print();
+	std::cout << std::endl;
+
 	return 0;
 }
