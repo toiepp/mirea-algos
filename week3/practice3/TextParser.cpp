e8bbe02561d62a2fac8c193d6a41c2dc21f5ba62
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2021-09-29 13:16:27 +0300

    make minimum frame for task3

---
+++ week3/practice3/TextParser.cpp
@@ -0,0 +1,17 @@
+//
+// Created by mikholskiyivan on 9/29/21.
+//
+
+#include "TextParser.h"
+TextParser::TextParser(std::string &s) : sentence(s) {}
+
+void TextParser::set_new(std::string &s) {
+	this->sentence = s;
+}
+
+std::string TextParser::find_longest_with_same_edges() {
+}
+
+int TextParser::find_last() {
+	return 0;
+}
---
+++ week3/practice3/TextParser.h
@@ -0,0 +1,24 @@
+#ifndef MIREA_ALGOS_TEXTPARSER_H
+#define MIREA_ALGOS_TEXTPARSER_H
+
+#include <string>
+
+class TextParser {
+	std::string sentence;
+
+public:
+	explicit TextParser(std::string &);
+	void set_new(std::string &);
+	/*
+	 * Дано предложение, состоящее из слов. Найти самое длинное
+	 * слово предложения, первая и последняя буквы которого
+	 * одинаковы*/
+	std::string find_longest_with_same_edges();
+	/*
+	 * Используя алгоритм Кнута-Мориса-Пратта, найти индекс
+	 * последнего вхождения образца в текст.*/
+	int find_last();
+};
+
+
+#endif
