5a22ee8e69d3daf4ef524dadc50978e6aa4a7ded
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2021-10-11 13:07:28 +0300

    change some tests

--- week3/practice3/main.cpp
+++ week3/practice3/main.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 
 int main() {
-	std::string s = "Lorem ipsui dolor sit amet consectetuc adipiscing elit "
+	std::string s = "Lorem ipsui aoba dolor sit amet consectetuc adipiscing elit "
 					"Quisque vel auctor ut nisl venenatis luctus Vivamus dolor libero "
-					"ultrices a elit at rhoncus consectetuc sem eros Orci varius natoque "
-					"penatibus et magnis dis parturient vel montes nascetur ridiculus mus "
+					"ultrices a elit at rhoncus consectetuc sem eros orci varius natoque "
+					"penatibus et magnis dis parvelturient vel montes nascetur ridiculus mus "
 					"Aenean sem lorem quam condimentum et urna viverra dignissim semper sem "
 					"Praesent eu lorem ut tellus sollicitudin luctus quis quis nibh Cras "
 					"fermentum ante sem eu tincidunt sfhgjkdhlgjdlghsdlgjfhsldfkjgshds sem mauris pulvinar ipsum "
@@ -16,13 +16,18 @@ int main() {
 					"conguehtrlksjgfc";
 
 	TextParser tp = TextParser(s);
+
 	std::cout << tp.find_longest_with_same_edges() << std::endl;
 	std::cout << tp.find_last("vel") << std::endl;
 
-	s = "dsjhfl dskjafhlskd flfghru  jalghfjkg gfualifua jg fguiafgrahdshaalkjgf";
+
+	s = "dsjhfl dskjafhjglskd  flfghru jalghfjkg gfualifua jg fguiafgrahdshaalkjgf";
 	tp.set_new(s);
 	std::cout << tp.find_longest_with_same_edges() << std::endl;
-	std::cout << tp.find_last("sem") << std::endl;
+	std::cout << tp.find_last("jg") << std::endl;
 
+	s = "abcabccabcabdcaabcabd";
+	tp.set_new(s);
+	std::cout << tp.find_last("abcabd") << std::endl;
 	return 0;
 }
