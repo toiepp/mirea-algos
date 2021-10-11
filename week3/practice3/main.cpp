679ae0dcadea38adab4e87a1a7a33fdae6b6b14b
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2021-10-11 11:27:02 +0300

    make test for Knuth–Morris–Pratt algorithm

--- week3/practice3/main.cpp
+++ week3/practice3/main.cpp
@@ -3,25 +3,26 @@
 
 int main() {
 	std::string s = "Lorem ipsui dolor sit amet consectetuc adipiscing elit "
-					"Quisque auctor ut nisl venenatis luctus Vivamus dolor libero "
-					"ultrices a elit at rhoncus consectetuc eros Orci varius natoque "
-					"penatibus et magnis dis parturient montes nascetur ridiculus mus "
-					"Aenean lorem quam condimentum et urna viverra dignissim semper sem "
+					"Quisque vel auctor ut nisl venenatis luctus Vivamus dolor libero "
+					"ultrices a elit at rhoncus consectetuc sem eros Orci varius natoque "
+					"penatibus et magnis dis parturient vel montes nascetur ridiculus mus "
+					"Aenean sem lorem quam condimentum et urna viverra dignissim semper sem "
 					"Praesent eu lorem ut tellus sollicitudin luctus quis quis nibh Cras "
-					"fermentum ante eu tincidunt sfhgjkdhlgjdlghsdlgjfhsldfkjgshds sem mauris pulvinar ipsum "
+					"fermentum ante sem eu tincidunt sfhgjkdhlgjdlghsdlgjfhsldfkjgshds sem mauris pulvinar ipsum "
 					"sed placerat nulla turpis vel velit Ut sattis nec turpis ac pharetra "
-					"Etiam venenatis hendrerit mauris quis pellentesque metus malesuada eu "
-					"Pellentesque a mattis justo Orci varius natoque penatibus et magnis dis "
-					"parturienp montes nascetur ridiculus mus Quisque arcu nibh accumsan et "
+					"Etiam sem venenatis hendrerit mauris sem quis pellentesque metus malesuada eu "
+					"Pellentesque vel a mattis vel justo Orci varius natoque penatibus et magnis dis "
+					"parturienp montes nascetur ridiculus mus Quisque arcu nibh sem accumsan et "
 					"conguehtrlksjgfc";
 
 	TextParser tp = TextParser(s);
 	std::cout << tp.find_longest_with_same_edges() << std::endl;
+	std::cout << tp.find_last("vel") << std::endl;
 
 	s = "dsjhfl dskjafhlskd flfghru  jalghfjkg gfualifua jg fguiafgrahdshaalkjgf";
 	tp.set_new(s);
 	std::cout << tp.find_longest_with_same_edges() << std::endl;
-
+	std::cout << tp.find_last("sem") << std::endl;
 
 	return 0;
 }
