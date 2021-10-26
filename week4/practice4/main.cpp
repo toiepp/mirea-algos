e78dbb9bbc492c3d59c95be4c87ce2e9b55381da
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2021-10-26 20:39:00 +0300

    task4: complete funtion creating a binary tree of search

--- week4/practice4/main.cpp
+++ week4/practice4/main.cpp
@@ -11,47 +11,80 @@
  * Найти высоту дерева
  */
 
+template<typename T>
 struct TreeNode {
-	std::string city;
+	TreeNode *parent = nullptr;
+	T data;
 	TreeNode *left = nullptr;
 	TreeNode *right = nullptr;
 
-	explicit TreeNode(std::string city) : city(std::move(city)) {}
-
-	TreeNode(std::string city, TreeNode *left, TreeNode *right)
-		: city(std::move(city)), left(left), right(right) {}
+	explicit TreeNode(T data) : data(std::move(data)) {}
 };
 
 // Функция создания бинарного дерева поиска с заданным
 // количеством элементов
-void build_binary_tree(TreeNode*, size_t);
+template<typename T>
+void build_binary_tree(TreeNode<T> *, const T &);
 
 // Функция вставки нового элемента
-void insert(TreeNode*, TreeNode*);
+template<typename T>
+void insert(TreeNode<T> *, TreeNode<T> *);
 
 // Функция симметричного обхода
-void symmetric_print(TreeNode*);
+template<typename T>
+void symmetric_print(TreeNode<T> *);
 
 // Функция обхода в ширину
-void wide_print(TreeNode*);
+template<typename T>
+void wide_print(TreeNode<T> *);
 
 // Функция вычисления длины пути от корня до заданного значения
-int get_length(TreeNode*, TreeNode*);
+template<typename T>
+int get_length(TreeNode<T> *, TreeNode<T> *);
 
 // Функция вычисления высоты дерева
-int get_height(TreeNode*);
+template<typename T>
+int get_height(TreeNode<T> *);
 
 int main() {
 	std::cout << "=== Практическая работа №4 ===" << std::endl;
 	std::cout << "===       Вариант 12       ===" << std::endl;
 
 	size_t n;
-	auto *root = new TreeNode("Root City");
-	std::cout << "Сколько элементов добавить в дерево: ";
+	int data;
+	std::cout << "Введите кол-во городов: ";
 	std::cin >> n;
 
-	// Строю бинарное дерево поиска
-	build_binary_tree(root, n);
+	std::cin >> data;
+
+	auto *root = new TreeNode<int>(data);
+
+	for (int i = 0; i < n - 1; ++i) {
+		std::cin >> data;
+		build_binary_tree(root, data);
+	}
 
 	return 0;
+}
+
+// 26 30 32 6 41 2 19 21 4 25 7 44 45 24 17 13 34 35 22 27
+// left[data] < this[data] <= right[data]
+template<typename T>
+void build_binary_tree(TreeNode<T> *root, const T &data) {
+	// Если узел уже имеет двух потомков или нужно сменить корневой текущий узел
+	if ((root->left && root->right) /*еще одно условие должно быть*/
+		|| (data > root->data && root->right)
+		|| (data <= root->data && root->left)) {
+		if (data < root->data) {
+			build_binary_tree(root->left, data);
+		} else {
+			build_binary_tree(root->right, data);
+		}
+		return;
+	}
+	if (data < root->data) {
+		root->left = new TreeNode(data);
+	} else {
+		root->right = new TreeNode(data);
+	}
 }
