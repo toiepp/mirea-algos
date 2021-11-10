39e8e5afc3e547079554c6e507fa533fcf9463df
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2021-11-10 17:21:02 +0300

    task5: report done

--- week5/practice5/main.cpp
+++ week5/practice5/main.cpp
@@ -9,149 +9,42 @@ struct {
 } typedef Graph;
 
 /*
- * Граф, содержащий Эйлеров цикл
-10
-a b c d e f g h i j
-0 1 1 0 1 1 0 0 0 0
-1 0 1 1 1 0 0 0 0 0
-1 1 0 1 0 0 0 0 1 0
-0 1 1 0 0 0 0 1 1 0
-1 1 0 0 0 1 1 0 0 0
-1 0 0 0 1 0 1 0 0 1
-0 0 0 0 1 1 0 1 0 1
-0 0 0 1 0 0 1 0 1 1
-0 0 1 1 0 0 0 1 0 1
-0 0 0 0 0 1 1 1 1 0
-*/
+	 * Граф, содержащий Эйлеров цикл
+	10
+	a b c d e f g h i j
+	0 1 1 0 1 1 0 0 0 0
+	1 0 1 1 1 0 0 0 0 0
+	1 1 0 1 0 0 0 0 1 0
+	0 1 1 0 0 0 0 1 1 0
+	1 1 0 0 0 1 1 0 0 0
+	1 0 0 0 1 0 1 0 0 1
+	0 0 0 0 1 1 0 1 0 1
+	0 0 0 1 0 0 1 0 1 1
+	0 0 1 1 0 0 0 1 0 1
+	0 0 0 0 0 1 1 1 1 0
+	*/
 
 /*
- * 1. Найти и вывести эйлеров цикл в графе.
- *
- * Эйлеров путь (эйлерова цепь) в графе — это путь,
- * проходящий по всем рёбрам графа и притом только
- * по одному разу.
- *
- * Эйлеров путь в графе существует тогда и только тогда,
- * когда граф связный и содержит не более двух вершин
- * нечётной степени.
- *
- * 2. Реализовать обход графа в ширину. +
- * */
+	 * 1. Найти и вывести эйлеров цикл в графе.
+	 *
+	 * Эйлеров путь (эйлерова цепь) в графе — это путь,
+	 * проходящий по всем рёбрам графа и притом только
+	 * по одному разу.
+	 *
+	 * Эйлеров путь в графе существует тогда и только тогда,
+	 * когда граф связный и содержит не более двух вершин
+	 * нечётной степени.
+	 *
+	 * 2. Реализовать обход графа в ширину. +
+	 * */
 
 // http://graphonline.ru/?graph=HDumlQSPEidmrFMJ
 
-bool deg(std::vector<bool> &vertex) {
-	return std::count(vertex.begin(), vertex.end(), true);
-}
+void euler_path(std::vector<std::vector<bool>> &graph,
+				std::vector<char> &nodes);
 
-void print_matrix(std::vector<Graph> &graph) {
-	std::cout << "  ";
-	std::for_each(graph.begin(), graph.end(), [](const Graph &v){
-		std::cout << v.sign << " ";
-	});
-	std::cout << std::endl;
-	for (int i = 0; i < graph.size(); ++i) {
-		std::cout << graph.at(i).sign << " ";
-		for (int j = 0; j < graph.size(); ++j) {
-			std::cout << graph.at(i).edges.at(j) << " ";
-		}
-		std::cout << std::endl;
-	}
-}
-
-void euler_path(std::vector<std::vector<bool>> &graph, std::vector<char> &nodes) {
-	// Проверяю, является ли граф связным
-	for (std::vector<bool> &vertex : graph) {
-		if (std::count(vertex.begin(), vertex.end(), 1) == 0) {
-			std::cout << "Граф не содержит Эйлеров цикл (граф несвязный)" << std::endl;
-			return;
-		}
-	}
-
-	// Проверяю, содержит ли граф вершины с нечетной степенью
-	int count = 0;
-	for (std::vector<bool> &vertex : graph) {
-		if (std::count(vertex.begin(), vertex.end(), true) % 2 == 1) count++;
-		if (count > 2) {
-			std::cout << "В графе более 2-х вершин с нечетной степенью ==> граф не содержит эйлеров путь." << std::endl;
-			return;
-		}
-	}
-
-	// Создаю обертку над графом для простоты вывода пути на экран
-	std::vector<Graph> g_vec(graph.size());
-	for (int i = 0; i < graph.size(); ++i) {
-		g_vec[i].sign = nodes.at(i);
-		g_vec[i].edges = graph.at(i);
-	}
-
-	// Если граф полуэйлеровый, то путь надо
-	// начинать с вершины с нечетной степенью
-	std::vector<char> result;
-	std::stack<Graph *> stack;
-
-	// Кладу первую попавшуюся
-	// вершину нечетной степени в стек
-	size_t idx_of_start = std::distance(graph.begin(),
-										std::find_if(graph.begin(), graph.end(),
-													 [](const std::vector<bool> &vertex) {
-														 return std::count(vertex.begin(), vertex.end(), true) % 2;
-													 }));
-	stack.push(&g_vec.at((idx_of_start != g_vec.size()) ? idx_of_start : 0));
-
-	while (!stack.empty()) {
-		Graph *V = stack.top();
-		bool found = false;
-		for (int i = 0; i < nodes.size(); ++i) {
-			if (V->edges.at(i)) {
-				found = true;
-				// Кладу другой конец этой вершины в стек
-				stack.push(&g_vec.at(i));
-				// Удаляю ребро, которое было пройдено
-				V->edges.at(i) = false;
-				g_vec.at(i).edges[std::distance(g_vec.begin(), std::find_if(g_vec.begin(), g_vec.end(), [&V](const Graph &G) {
-													return G.sign == V->sign;
-												}))] = false;
-				break;
-			}
-		}
-		if (!found) {
-			stack.pop();
-			result.push_back(V->sign);
-		}
-	}
-
-	print_matrix(g_vec);
-
-	std::for_each(result.begin(),
-				  result.end() - 1,
-				  [](const char &vertex) {
-					  std::cout << vertex << " --> ";
-				  });
-	std::cout << *(result.end() - 1) << std::endl;
-}
-
-void level_order(/* матрица смежности графа */ std::vector<std::vector<bool>> &graph,
-				 /* имена вершин графа */ std::vector<char> &nodes) {
-	bool visited[nodes.size()];// массив вершин, которые уже обошли
-	std::queue<std::vector<bool>> queue;
-	queue.push(graph.at(0));
-	visited[0] = true;
-	std::cout << nodes.at(0) << " ";
-
-	while (!queue.empty()) {
-		std::vector<bool> vertex = queue.front();
-		queue.pop();
-		for (int i = 0; i < vertex.size(); ++i) {
-			// если у вершины есть ребро с i-й вершиной и ее ещё не обошли
-			if (vertex.at(i) && !visited[i]) {
-				std::cout << nodes.at(i) << " ";// вывожу имя этой вершины
-				queue.push(graph.at(i));        // кладу эту вершину в очередь
-				visited[i] = true;
-			}
-		}
-	}
-}
+void breadth_first_traversal(std::vector<std::vector<bool>> &graph,
+							 std::vector<char> &nodes);
 
 int main() {
 	std::cout << "=== Практическая 5 ===" << std::endl;
@@ -213,8 +106,112 @@ int main() {
 	std::cout << std::endl;
 
 	std::cout << "=== Обход в ширину ===" << std::endl;
-	level_order(graph, nodes);
+	breadth_first_traversal(graph, nodes);
 	std::cout << std::endl;
 
 	return 0;
+}
+
+void euler_path(std::vector<std::vector<bool>> &graph, std::vector<char> &nodes) {
+	// Проверяю, является ли граф связным
+	for (std::vector<bool> &vertex : graph) {
+		if (std::count(vertex.begin(), vertex.end(), 1) == 0) {
+			std::cout << "Граф не содержит Эйлеров цикл (граф несвязный)"
+					  << std::endl;
+			return;
+		}
+	}
+
+	// Проверяю, содержит ли граф вершины с нечетной степенью
+	int count = 0;
+	for (std::vector<bool> &vertex : graph) {
+		if (std::count(vertex.begin(), vertex.end(), true) % 2 == 1) {
+			count++;
+		}
+		if (count > 2) {
+			std::cout << "В графе более 2-х вершин с нечетной степенью ==> "
+					  << std::endl
+					  << " ==> граф не содержит эйлеров путь." << std::endl;
+			return;
+		}
+	}
+
+	// Создаю обертку над графом для простоты вывода пути на экран
+	std::vector<Graph> g_vec(graph.size());
+	for (int i = 0; i < graph.size(); ++i) {
+		g_vec[i].sign = nodes.at(i);
+		g_vec[i].edges = graph.at(i);
+	}
+
+	// Если граф полуэйлеровый, то путь надо
+	// начинать с вершины с нечетной степенью
+	std::stack<Graph *> stack;
+	std::vector<char> result;
+
+	// Кладу первую попавшуюся
+	// вершину нечетной степени в стек
+	size_t idx_of_start =
+			std::distance(graph.begin(),
+						  std::find_if(
+								  graph.begin(),
+								  graph.end(),
+								  [](const std::vector<bool> &vertex) {
+									  return std::count(vertex.begin(), vertex.end(), true) % 2;
+								  }));
+
+	stack.push(&g_vec.at((idx_of_start != g_vec.size()) ? idx_of_start : 0));
+
+	while (!stack.empty()) {
+		Graph *V = stack.top();
+		bool found = false;
+		for (int i = 0; i < nodes.size(); ++i) {
+			if (V->edges.at(i)) {
+				found = true;
+				// Кладу другой конец этой вершины в стек
+				stack.push(&g_vec.at(i));
+				// Удаляю ребро, которое было пройдено
+				V->edges.at(i) = false;
+				g_vec.at(i).edges[std::distance(g_vec.begin(),
+												std::find_if(g_vec.begin(), g_vec.end(),
+															 [&V](const Graph &G) {
+																 return G.sign == V->sign;
+															 }))] = false;
+				break;
+			}
+		}
+		if (!found) {
+			stack.pop();
+			result.push_back(V->sign);
+		}
+	}
+
+	std::for_each(result.begin(),
+				  result.end() - 1,
+				  [](const char &vertex) {
+					  std::cout << vertex << " --- ";
+				  });
+	std::cout << *(result.end() - 1) << std::endl;
+}
+
+void breadth_first_traversal(std::vector<std::vector<bool>> &graph,
+				 std::vector<char> &nodes) {
+
+	bool visited[nodes.size()];
+
+	std::queue<std::vector<bool>> queue;
+	queue.push(graph.at(0));
+	visited[0] = true;
+	std::cout << nodes.at(0) << " ";
+
+	while (!queue.empty()) {
+		std::vector<bool> vertex = queue.front();
+		queue.pop();
+		for (int i = 0; i < vertex.size(); ++i) {
+			if (vertex.at(i) && !visited[i]) {
+				std::cout << nodes.at(i) << " ";
+				queue.push(graph.at(i));
+				visited[i] = true;
+			}
+		}
+	}
 }
